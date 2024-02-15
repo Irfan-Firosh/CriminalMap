59d902b2cf6917e5282dffa475fd2c10204fee9f
Irfan Firosh <irfan.firosh@gmail.com>
2024-02-15 02:15:50 -0500

    Updated code, to scrape data for last 11 weeks

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 import requests
 import csv
 
-def exportCsv(URL):
+def exportCsv(URL, mode):
+
+    # Only creating a new file
+    if (URL == "create"):
+        with open('crimeData.csv', 'w', newline='') as file:
+            pass
+        return
+
     try:
         # Send HTTP request
         URL_RESP = requests.get(URL)
@@ -14,10 +22,9 @@ def exportCsv(URL):
 
             # Finding tables with specific requirements
             tables = soup.find_all('table')
-            print(len(tables))
 
             # Attempting to export to csv
-            with open('crimeData.csv', 'w', newline='') as file:
+            with open('crimeData.csv', mode, newline='') as file:
                 writer = csv.writer(file)
 
                 # Loop to write the scraped data into the csv file
@@ -27,10 +34,36 @@ def exportCsv(URL):
                         columns = row.find_all('td')
                         rowData = [column.text.strip() for column in columns]
                         writer.writerow(rowData)
-                
-                print("Data was successfully exported")
+                return 1
 
     except Exception as e:
-        print("An error occurred:", e)
+        print("An error occurred in:", URL)
+        return 0
 
-exportCsv("https://www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/020524-daily-crime-log.php")
+def caller():
+    current = datetime.now()
+    recentMonday = current- timedelta(current.weekday())
+
+    base1 = "https://www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/"
+    base2 = "-daily-crime-log.php"
+
+    status = 0
+
+    exportCsv("create", 'a')
+
+    for i in range(1,12):
+        mondays = recentMonday - timedelta(weeks=i)
+        formattedMonday = mondays.strftime("%m%d%y")
+        url = base1 + formattedMonday + base2
+        exportCsv(url, 'a')
+        if exportCsv == 0:
+            print("Error in: " + url)
+            status += 1
+    
+    if status == 0:
+        print("Success!")
+
+
+caller()
+
+
